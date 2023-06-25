fc3cfd65b5102ed92185593185113467f979f45d
yvan99 <ishimweyvan90@gmail.com>
2023-06-25 14:37:37 +0200

    add auth feature

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,25 @@
 import asyncHandler from "express-async-handler";
 import User from "../Models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Authentication successful" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      message:"Login successful",
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -12,13 +29,14 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error(`User ${name} already exists`);
   }
-  const user = User.create({ name, email, password });
+  const user = await User.create({ name, email, password });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       message: "User registered successfully",
-      _id: user._id,
-      name: user.name,
+      id: user._id,
+      names: user.name,
       email: user.email,
     });
   } else {
